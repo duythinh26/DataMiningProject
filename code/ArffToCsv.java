45f8976fa2edb616ab3d389f902493a7a125c7e1
duythinh26 <pduythinh79@gmail.com>
2023-04-20 20:01:54 +0700

    create ArffToCsv file

--- code/ArffToCsv.java
+++ code/ArffToCsv.java
@@ -1,7 +1,18 @@
+import java.io.File;
+
+import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.converters.CSVSaver;
 
 public class ArffToCsv {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws Exception {
+        ArffLoader arffLoader = new ArffLoader();
+        arffLoader.setSource(new File("./code/data/HepatitisCdata.arff"));
+        Instances dataset = arffLoader.getDataSet();
+
+        CSVSaver csvSaver = new CSVSaver();
+        csvSaver.setInstances(dataset);
+        csvSaver.setFile(new File("./code/data/HepatitisCdata1.csv"));
+        csvSaver.writeBatch();
     }
 }

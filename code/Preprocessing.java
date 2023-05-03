37b57c76ed1c5c5dddd740ee3a631b749f592ee0
duythinh26 <pduythinh79@gmail.com>
2023-05-03 21:51:58 +0700

    update Preprocessing.java

--- code/Preprocessing.java
+++ code/Preprocessing.java
@@ -6,20 +6,52 @@ import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Discretize;
 import weka.filters.unsupervised.attribute.InterquartileRange;
-import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.Remove;
 import weka.filters.unsupervised.attribute.ReplaceMissingValues;
+import weka.filters.unsupervised.attribute.StringToNominal;
 import weka.filters.unsupervised.instance.RemoveWithValues;
 
 public class Preprocessing {
     public static void main(String[] args) throws Exception{
-        /**
-         * Handle missing values
+        /*
+         * Convert string to nominal
          */
         // Load raw dataset
         DataSource src = new DataSource("./code/data/HepatitisCdata.arff");
         Instances dataset = src.getDataSet();
         ArffSaver saver = new ArffSaver();
+
+        int[] replacesAttrs = new int[] { 4, 5, 6, 10, 13 };
+        for (int i = 0; i < dataset.numInstances(); i++) {
+            for (int j = 0; j < replacesAttrs.length; j++) {
+                if (dataset.instance(i).toString(replacesAttrs[j]).equals("NA")) {
+                    dataset.instance(i).setMissing(replacesAttrs[j]);
+                    ;
+                }
+            }
+        }
+
+        // Create object to convert string to nominal
+        StringToNominal StringToNominal = new StringToNominal();
+
+        // Set the filter option
+        StringToNominal.setOptions(new String[]{"-R", "first-last"});
+
+        // Put the dataset into the filter and use filter
+        StringToNominal.setInputFormat(dataset);
+        Instances string2Nominal = Filter.useFilter(dataset, StringToNominal);
+
+        // Write a new dataset after finish handling missing values
+        saver.setInstances(string2Nominal);
+        saver.setFile(new File("./code/data/StringToNominal-HepatitisCdata.arff"));
+        saver.writeBatch();
+
+        /**
+         * Handle missing values
+         */
+
+        src = new DataSource("./code/data/StringToNominal-HepatitisCdata.arff");
+        dataset = src.getDataSet();
 
         // Create object to handle missing values
         ReplaceMissingValues missingValues = new ReplaceMissingValues();
@@ -33,50 +65,22 @@ public class Preprocessing {
         saver.setFile(new File("./code/data/noMissing-HepatitisCdata.arff"));
         saver.writeBatch();
 
-        /*
-         * Convert numeric to nominal
-         */
-        // Load raw dataset
-        src = new DataSource("./code/data/noMissing-HepatitisCdata.arff");
-        dataset = src.getDataSet();
-
-        // Set up the options to convert from numeric to nominal
-        String[] num2NomOption = new String[]{"-R", "first-last"};
-
-        // Create object to convert numeric to nominal
-        NumericToNominal numericToNominal = new NumericToNominal();
-
-        // Set the filter option
-        numericToNominal.setOptions(num2NomOption);
-
-        // Put the dataset into the filter and use filter
-        numericToNominal.setInputFormat(dataset);
-        Instances numeric2Nominal = Filter.useFilter(dataset, numericToNominal);
-
-        // Write a new dataset after finish handling missing values
-        saver.setInstances(numeric2Nominal);
-        saver.setFile(new File("./code/data/numericToNominal-HepatitisCdata.arff"));
-        saver.writeBatch();
-
         /**
          * Detect outliers and extreme values
          */
         // Load no missing values dataset
-        DataSource iqrSrc = new DataSource("./code/data/numericToNominal-HepatitisCdata.arff");
-        Instances iqrDataset = iqrSrc.getDataSet();
-
-        // Set up the options to interquartile range
-        String[] option = new String[]{"-R", "first-last", "-O", "1.5", "-E", "3.0"};
+        src = new DataSource("./code/data/noMissing-HepatitisCdata.arff");
+        dataset = src.getDataSet();
 
         // Create an object to define interquartile range values
-        InterquartileRange interquartileRange = new InterquartileRange();
+        InterquartileRange range = new InterquartileRange();
 
         // Set the filter option
-        interquartileRange.setOptions(option);
+        range.setOptions(new String[]{"-R", "first-last", "-O", "3.0", "-E", "6.0"});
 
         // Put the dataset into the filter and use filter
-        interquartileRange.setInputFormat(iqrDataset);
-        Instances iqrData = Filter.useFilter(iqrDataset, interquartileRange);
+        range.setInputFormat(dataset);
+        Instances iqrData = Filter.useFilter(dataset, range);
 
         // Write a new dataset after detect interquartile range values
         saver.setInstances(iqrData);
@@ -87,8 +91,8 @@ public class Preprocessing {
          * Remove the outliers and extreme values
          */
         // Load InterQuartile Range values dataset
-        DataSource outlierSrc = new DataSource("./code/data/IQR-HepatitisCdata.arff");
-        Instances outlierDataset = outlierSrc.getDataSet();
+        src = new DataSource("./code/data/IQR-HepatitisCdata.arff");
+        dataset = src.getDataSet();
 
         // Set up the options to remove outlier values
         String[] outlierOption = new String[]{"-S", "0.0", "-C", "15", "-L", "last"};
@@ -100,8 +104,8 @@ public class Preprocessing {
         removeOutlier.setOptions(outlierOption);
 
         // Put the dataset into the filter and use filter
-        removeOutlier.setInputFormat(outlierDataset);
-        Instances removeOutlierData = Filter.useFilter(outlierDataset, removeOutlier);
+        removeOutlier.setInputFormat(dataset);
+        Instances removeOutlierData = Filter.useFilter(dataset, removeOutlier);
 
         // Write a new dataset after remove outlier values
         saver.setInstances(removeOutlierData);
@@ -109,8 +113,8 @@ public class Preprocessing {
         saver.writeBatch();
 
         // Load outlier removed dataset
-        DataSource extremeSrc = new DataSource("./code/data/outlierRemoved-HepatitisCdata.arff");
-        Instances extremeDataset = extremeSrc.getDataSet();
+        src = new DataSource("./code/data/outlierRemoved-HepatitisCdata.arff");
+        dataset = src.getDataSet();
 
         // Set up the options to remove extreme values
         String[] extremeOption = new String[]{"-S", "0.0", "-C", "16", "-L", "last"};
@@ -122,8 +126,8 @@ public class Preprocessing {
         removeExtreme.setOptions(extremeOption);
 
         // Put the dataset into the filter and use filter
-        removeExtreme.setInputFormat(extremeDataset);
-        Instances removeExtremeData = Filter.useFilter(extremeDataset, removeExtreme);
+        removeExtreme.setInputFormat(dataset);
+        Instances removeExtremeData = Filter.useFilter(dataset, removeExtreme);
 
         // Write a new dataset after remove extreme values
         saver.setInstances(removeExtremeData);
@@ -131,13 +135,13 @@ public class Preprocessing {
         saver.writeBatch();
 
         /**
-         * Remove Outlier and ExtremeValue attributes
+         * Remove unnecessary attributes
          */
         src = new DataSource("./code/data/extremeRemoved-HepatitisCdata.arff");
         dataset = src.getDataSet();
 
         // Set up the options to remove attribute
-        String[] opt = new String[]{"-R", "15,16"};
+        String[] opt = new String[]{"-R", "1,15,16"};
 
         // Create an object to remove attribute
         Remove remove = new Remove();
@@ -158,8 +162,8 @@ public class Preprocessing {
          * Discretize Attributes
          */
         // Load dataset
-        DataSource discretizeSrc = new DataSource("./code/data/cleaned-HepatitisCdata.arff");
-        Instances discretizeDataset = discretizeSrc.getDataSet();
+        src = new DataSource("./code/data/cleaned-HepatitisCdata.arff");
+        dataset = src.getDataSet();
 
         // Set up options to findNumBins, 10 bins, -1.0 desiredWeightOfInstancesPerInterval, 6 binRangePrecision
         String[] discretizeOption = new String[]{"-O", "-B", "10", "-M", "-1.0", "-R", "first-last", "-precision", "6"};
@@ -171,8 +175,8 @@ public class Preprocessing {
         discretize.setOptions(discretizeOption);
 
         // Put the dataset into the filter and use filter
-        discretize.setInputFormat(discretizeDataset);
-        newData = Filter.useFilter(discretizeDataset, discretize);
+        discretize.setInputFormat(dataset);
+        newData = Filter.useFilter(dataset, discretize);
 
         // Write a new dataset after discretize
         saver.setInstances(newData);

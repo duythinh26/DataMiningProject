9fa2731518493f2e06f973fb5a6807c970b22e68
TranHuuNhat <96304020+thn3032001@users.noreply.github.com>
2023-05-03 15:56:56 +0700

    Update Classifier.java

--- Classifier.java
+++ Classifier.java
@@ -1,24 +1,39 @@
+import weka.attributeSelection.AttributeSelection;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GreedyStepwise;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.classifiers.trees.J48;
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
 
-public class J48Classifier {
+public class MyClassifier {
     public static void main(String[] args) throws Exception {
+        // Load the dataset
         DataSource source = new DataSource("./code/data/HepatitisCdata.arff");
         Instances data = source.getDataSet();
-        data.setClassIndex(data.numAttributes() - 1);
-        J48 j48 = new J48();
-        
-        Evaluation eval = new Evaluation(data);
-        eval.crossValidateModel(j48, data, 10, new Random(1));
+        data.setClassIndex(data.numAttributes() - 1); // Set the class attribute
+
+        // Perform feature selection using CFS and GreedyStepwise
+        AttributeSelection selector = new AttributeSelection();
+        CfsSubsetEval evaluator = new CfsSubsetEval();
+        GreedyStepwise search = new GreedyStepwise();
+        selector.setEvaluator(evaluator);
+        selector.setSearch(search);
+        selector.SelectAttributes(data);
+        int[] indices = selector.selectedAttributes();
+
+        // Use J48 as the classifier
+        Classifier classifier = new J48();
+
+        // Train the classifier on the selected features
+        Instances selectedData = new Instances(data);
+        selectedData.deleteAttributes(indices);
+        classifier.buildClassifier(selectedData);
+
+        // Evaluate the classifier
+        Evaluation eval = new Evaluation(selectedData);
+        eval.crossValidateModel(classifier, selectedData, 10, new java.util.Random(1)); // 10-fold cross validation
         System.out.println(eval.toSummaryString());
-        j48.buildClassifier(data);
-        
-        Instances test = 
-        for (int i = 0; i < test.numInstances(); i++) {
-            double pred = j48.classifyInstance(test.instance(i));
-            System.out.println("Prediction for instance " + i + ": " + data.classAttribute().value((int) pred));
-        }
     }
 }

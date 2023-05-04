ebd7e6377aac56230fa68c090b8b2180b9259529
Nguyễn Thanh Thanh Tùng <nguyenththanhtung317@gmail.com>
2023-05-04 22:23:36 +0700

    Update Classification.java
    remove unnecessary comments

--- code/Classification.java
+++ code/Classification.java
@@ -116,17 +116,9 @@ public class Classification {
         System.out.println(eval.toMatrixString("=== Confusion matrix for fold ===\n"));
         // Print out evaluation results
         System.out.println("Correct % = " + eval.pctCorrect());
-        // System.out.println("Incorrect % = "+eval.pctIncorrect());
-        // System.out.println("AUC = "+eval.areaUnderROC(1));
-        // System.out.println("kappa = "+eval.kappa());
-        // System.out.println("MAE = "+eval.meanAbsoluteError());
-        // System.out.println("RMSE = "+eval.rootMeanSquaredError());
-        // System.out.println("RAE = "+eval.relativeAbsoluteError());
-        // System.out.println("RRSE = "+eval.rootRelativeSquaredError());
         System.out.println("Precision = " + eval.precision(1));
         System.out.println("Recall = " + eval.recall(1));
         System.out.println("fMeasure = " + eval.fMeasure(1));
-        // System.out.println("Error Rate = "+eval.errorRate());
         System.out.println("*********");
         System.out.println(eval.toSummaryString());
     }
